8403e05b5d98adf02ca5087b568bd8593f32b336
Glyucool <153933075+Glyucool@users.noreply.github.com>
2024-01-21 20:39:57 +0200

    Add files via upload

---
+++ homework9_1.js
@@ -0,0 +1,17 @@
+  // Функція для перемішування масиву
+function myBlend(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      // Обмін елементів
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  
+  // Початковий масив
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  
+  // Виклик функції перемішування
+  myBlend(arr);
+  
+  // Вивід результату
+  console.log(arr);
